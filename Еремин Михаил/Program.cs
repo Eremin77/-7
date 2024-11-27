aa0c15c5753e3c7d4e2629716813560890de882b
Eremin77 <ereminm828@gmail.com>
2024-11-27 15:48:24 +0300

    Add files via upload

---
+++ Еремин Михаил/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ЕреминМихаил
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Доброе утро! Я ваш фитнес-бот. Готовы к тренировкам? (введите 'выход' для выхода)");
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "выход")
+                {
+                    Console.WriteLine("Удачи! Не забывайте про активность!");
+                    break;
+                }
+
+                string response = GenerateMotivation();
+                Console.WriteLine(response);
+            }
+        }
+
+        static string GenerateMotivation()
+        {
+            string[] motivations = {
+                "Двигайтесь к своей цели, не останавливайтесь!",
+                "Каждый день - это новый шанс стать лучше!",
+                "Никогда не сдавайтесь, вы на правильном пути!",
+                "Помните, маленькие шаги ведут к большим достижениям!"
+            };
+
+            Random rand = new Random();
+            return motivations[rand.Next(motivations.Length)];
+        }
+    }
+}
